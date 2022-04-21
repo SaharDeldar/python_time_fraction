e9a70da80bde9507f4984345ae40ff50d1c88156
Sahar <100313500+sahardeldar@users.noreply.github.com>
2022-04-21 15:28:31 +0430

    Add files via upload

--- Time.py
+++ Time.py
@@ -15,7 +15,7 @@ class Time:
             print(" error kamtar az 0 ")
     def s_to_h(self):
       self.hour= self.second/3600
-      self.minute=s/60
+      self.minute=self.second/60
     def h_to_s(self):
         s= self.hour*3600
         s=self.minute*60
